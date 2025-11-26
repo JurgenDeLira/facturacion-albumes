b53731f810e73bef67233766e00c52c07ee07e87
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-11-26 12:48:52 -0700

    creación proyecto

---
+++ src/main/java/com/jorge/facturacion_vinilos/FacturacionVinilosApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.facturacion_vinilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionVinilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionVinilosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorge/facturacion_vinilos/FacturacionVinilosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.facturacion_vinilos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionVinilosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
