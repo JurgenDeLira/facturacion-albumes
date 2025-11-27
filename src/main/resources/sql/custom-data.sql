7aeee747a4ea43be3d14c4c5e2a77d9d49d87a77
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-11-27 11:45:20 -0700

    custom-data.sql

---
+++ src/main/resources/sql/custom-data.sql
@@ -0,0 +1,13 @@
+USE `facturacion-albumes`;
+INSERT INTO albumes (titulo, artista, anio_De_Lanzamiento, genero, anio_De_Edicion, discografica,
+formato, detalle, precio, fecha_creado)
+VALUES
+('Joes Garage Acts I, II & III', 'Frank Zappa', 1987,
+'Avantgarde, Parody, Art Rock, Prog Rock', 2016, 'Zappa Records', 'Vinilo',
+'3 x Vinilo, Album, Compilation, Reissue, Remastered, 180g', 1000, '2025-02-16 01:22:00'),
+('The Low End Theory', 'A Tribe Called Quest', 1991,
+'Conscious, Jazzy Hip-Hop, Boom Bap', 2025, 'RCA', 'Vinilo',
+'2 x Vinilo, LP, Album, Record Store Day, Limited Edition, Reissue, Green & Red Splatter', 900, '2025-02-16 01:22:00'),
+('Lola Versus Powerman And The Moneygoround, Part One', 'The Kinks', 1970,
+'Power Pop, Pop Rock, Psychedelic Rock', 2020, 'BMG', 'Vinilo',
+'Album, Reissue, Remastered, Stereo, 50th Anniversary', 700, '2025-02-16 01:22:00')
